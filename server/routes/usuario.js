acebec771625e1ac0e5d3ea220fbdb3c9e367694
luisdev97 <amadormontorodavid@gmail.com>
2019-08-09 11:49:48 +0200

    Get usuarios

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,6 +3,7 @@ const app = express();
 
 //encriptaremos el password de nuestros usuarios antes de realizar el POST
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
 const Usuario = require('../models/Usuario');
 
 
@@ -11,11 +12,30 @@ app.get('/', (req, res) => {
 });
 
 
+
 app.get('/usuarios', (req, res) => {
-    res.json({
-        usuarios: []
-    });
+
+    let desde = Number(req.query.desde) || 0;
+    let limite = Number(req.query.limite) || 5;
+    //Podemos indicar condiciones en el find {google: true}
+    Usuario.find({})
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                usuarios
+            });
+        })
 });
+
 
 
 app.post('/usuarios', (req, res) => {
@@ -48,12 +68,33 @@ app.post('/usuarios', (req, res) => {
 });
 
 
+
+//Para actualizar podemos usar el modelo Usuario y el metodo findById para traernos el usuario y dentro el Usuario.save
+//Otra forma es usar findByIdAndUpdate(id,(err,usuarioDB));
 app.put('/usuarios/:id', (req, res) => {
     let id = req.params.id;
-    res.json({
-        id
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    //Para recibir el nuevo documento en lugar del original tras realizar los cambios, para esto pasamos como 3 argumento un objeto que contenga new: true 
+    //Para evitar que se actualicen campos indebidos en este momento agregarmos al 3 parametro runValidators : true
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+
+        if (err) {
+            res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+
     });
+
 });
+
 
 
 app.delete('/usuarios/:id', (req, res) => {
@@ -62,5 +103,6 @@ app.delete('/usuarios/:id', (req, res) => {
 
     res.json(`delete usuario: ${id}`);
 });
+
 
 module.exports = app;
